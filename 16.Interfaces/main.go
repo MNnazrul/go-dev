2cc54f54c826e3ccbb78aba7dad565c5cdbe3c3d
MNnazrul <mnnazrulislam196130@gmail.com>
2025-03-13 21:32:04 +0600

    interface

---
+++ 16.Interfaces/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"io"
+)
+
+type Human struct {
+	Firstname string
+	Lastname string
+	Age int
+	Country string
+}
+
+type DomesticAnimal interface {
+	ReceiveAffection(from Human)
+	GivenAffection(to Human)
+}
+
+type Cat struct {
+	Name string
+}
+
+type Dog struct {
+	Name string
+}
+
+func (c Cat) ReceiveAffection(from Human) {
+	fmt.Printf("The cat named %s has received affection from Human named %s", c.Name, from.Firstname)
+}
+
+func (c Cat) GivenAffection(to Human) {
+	fmt.Printf("The cat named %s has given affection to Human %s", c.Name, to.Firstname)
+}
+
+func (d Dog) ReceiveAffection(from Human) {
+    fmt.Printf("The dog named %s has received affection from Human named %s\n", d.Name, from.Firstname)
+}
+
+func (d Dog) GiveAffection(to Human) {
+    fmt.Printf("The dog named %s has given affection to Human named %s\n", d.Name, to.Firstname)
+}
+
+func main() {
+	var r io.Reader
+	fmt.Println(r)
+}
