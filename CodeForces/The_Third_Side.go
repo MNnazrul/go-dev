9eb9cc04b4fe3087689947ab8479fbf131a4eac8
MNnazrul <mnnazrulislam196130@gmail.com>
2025-03-22 21:39:12 +0600

    f

--- CodeForces/The_Third_Side.go
+++ CodeForces/The_Third_Side.go
@@ -1,26 +1,30 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
+
+var reader = bufio.NewReader(os.Stdin)
 
 func solve() {
 	var n int
-	fmt.Scan(&n)
+	fmt.Fscanf(reader, "%d", &n)
 	ans := -n + 1
 	for n > 0 {
 		n--
 		var val int
-		fmt.Scan(&val)
+		fmt.Fscanf(reader, "%d", &val)
 		ans += val
 	}
-
+	
 	fmt.Println(ans)
 }
 
 func main() {
 	var t int
-	fmt.Scan(&t)
+	fmt.Fscanf(reader, "%d", &t)
 	for t > 0 {
 		t--
 		solve()
