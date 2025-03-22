0f91aeb79bc705c5ce001bb938169920b61fdf9f
MNnazrul <mnnazrulislam196130@gmail.com>
2025-03-22 20:39:15 +0600

    taking inpts

--- take-input/main.go
+++ take-input/main.go
@@ -1,42 +1,63 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
 )
 
+
 func main() {
-	
-	file, err := os.Open("input.txt")
+    file, err := os.Open("input.txt")
 
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer file.Close()
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    defer file.Close()
 
-	reader := bufio.NewReader(file)
 
-	for {
-		line, err := reader.ReadString('\n')
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
+    var t, val, val1 int
+    var str string 
 
-		fmt.Println(line)
-	}
+    fmt.Fscanf(file, "%d", &t)
 
-	// data := make([]byte, 100)
+    fmt.Println(t)
+    for ; t > 0; t-- {
+        fmt.Fscanf(file, "%s %d", &str, &val)
+        fmt.Println(str, val)
 
-	// _, err = reader.Read(data)
+        for ; val > 0; val-- {
+            fmt.Fscanf(file, "%d", &val1)
+            fmt.Print(val1, " ")
+        }
+        fmt.Println()
 
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	return
-	// }
+    }
 
-	// fmt.Println(string(data))
 
+    // reader := bufio.NewReader(file)
+
+
+
+    // for {
+    //     line, err := reader.ReadString('\n')
+    //     if err != nil {
+    //         fmt.Println(err)
+    //         return
+    //     }
+
+    //     results := strings.TrimSpace(line)
+    //     fmt.Println("=> ", results[0])
+    // }
+
+    // data := make([]byte, 100)
+
+    // _, err = reader.Read(data)
+
+    // if err != nil {
+    //     fmt.Println(err)
+    //     return
+    // }
+
+    // fmt.Println(string(data))
 }
