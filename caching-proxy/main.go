1fdbef01a4ed28237e0c887ced1fb9a24ccdf6a8
MNnazrul <mnnazrulislam196130@gmail.com>
2025-06-22 13:35:03 +0600

    cache-memory-added

--- caching-proxy/main.go
+++ caching-proxy/main.go
@@ -4,16 +4,24 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/MNnazrul/caching-proxy/cache"
 	"github.com/MNnazrul/caching-proxy/cli"
+	"github.com/MNnazrul/caching-proxy/proxy"
 )
 
 func main() {
 	args := cli.ParseArgs()
+	memCache := cache.NewMemoryCache()
 
 	if args.ClearCache {
-		// Implement cache clearer logic
+		memCache.Clear()
 		fmt.Println("Cache clearer (Placeholder).")
 		os.Exit(0)
 	}
 	fmt.Printf("Starting proxy on port %d, origin: %s\n", args.Port, args.Origin)
+	err := proxy.StartProxy(args.Port, args.Origin, memCache)
+	if err != nil {
+		fmt.Println("Error starting proxy:", err)
+		os.Exit(1)
+	}
 }
--- caching-proxy/proxy/server.go
+++ caching-proxy/proxy/server.go
@@ -5,15 +5,31 @@ import (
 	"io"
 	"net/http"
 	"net/url"
+
+	"github.com/MNnazrul/caching-proxy/cache"
 )
 
-func StartProxy(port int, origin string) error {
+func StartProxy(port int, origin string, memCache *cache.MemoryCache) error {
 	originURL, err := url.Parse(origin)
 	if err != nil {
 		return fmt.Errorf("invalid origin URL: %w", err)
 	}
 
 	handler := func(w http.ResponseWriter, r *http.Request) {
+		cacheKey := r.URL.Path + "?" + r.URL.RawQuery
+
+		if cached, ok := memCache.Get(cacheKey); ok {
+			for k, v := range cached.Header {
+				for _, vv := range v {
+					w.Header().Add(k, vv)
+				}
+			}
+			w.Header().Set("X-Cache", "HIT")
+			w.WriteHeader(cached.StatusCode)
+			w.Write(cached.Body)
+			return
+		}
+
 		proxyURL := *originURL
 		proxyURL.Path = r.URL.Path
 		proxyURL.RawQuery = r.URL.RawQuery
@@ -31,6 +47,18 @@ func StartProxy(port int, origin string) error {
 			return
 		}
 		defer resp.Body.Close()
+
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			http.Error(w, "Falied to read response", http.StatusInternalServerError)
+			return
+		}
+
+		memCache.Set(cacheKey, cache.CachedResponse{
+			StatusCode: resp.StatusCode,
+			Header:     resp.Header.Clone(),
+			Body:       body,
+		})
 
 		for k, v := range resp.Header {
 			for _, vv := range v {
